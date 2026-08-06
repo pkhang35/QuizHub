e070e0e1bcab4bbcefc2dad6cdd4d48d3ff8407a
pkhang35 <pkhang353@gmail.com>
2026-08-06 21:57:45 +0700

    add layout and pages

--- quiz-hub/src/layout/LayoutDefault/index.js
+++ quiz-hub/src/layout/LayoutDefault/index.js
@@ -1,42 +1,19 @@
-import {  NavLink, Outlet } from "react-router-dom"
+import {  Link, NavLink, Outlet } from "react-router-dom"
 import "./LayoutDefault.scss"
 function LayoutDefault(){
-    const navLickActive=(e)=>{
-        console.log(e)
-        return e.isActive ? "menu__link menu__link--active": "menu__link"
-    }
+    // const navLickActive=(e)=>{
+    //     console.log(e)
+    //     return e.isActive ? "menu__link menu__link--active": "menu__link"
+    // }
     return(
         <>
-
         <div className="layout-default">
            <header className="layout-default__header">
             <div className="layout-default__logo">Logo</div>
             <div className="menu">
                 <ul>
                     <li>
-                        <NavLink to="/" className={navLickActive}>Home</NavLink>
-                    </li>
-                    <li>
-                        <NavLink to="/about"  className={navLickActive}>About</NavLink>
-                    </li>
-                    <li>
-                        <NavLink to="/contact"  className={navLickActive}>Contact</NavLink>
-                    </li>
-                    <li>
-                        <NavLink to="/blog"  className={navLickActive}>Blog</NavLink>
-                        <ul className="menu__sub">
-                            <li>
-                                <NavLink to="/blog/news"  
-                                className={navLickActive}>News</NavLink>
-                            </li>
-                            <li>
-                                <NavLink to="/blog/related"  
-                                className={navLickActive}>Related</NavLink>
-                            </li>
-                        </ul>
-                    </li>
-                    <li>
-                        <NavLink to="/info-user"  className={navLickActive}>InfoUser</NavLink>
+                        <Link to="/">Home</Link>
                     </li>
                 </ul>
             </div>
--- quiz-hub/src/routes/index.js
+++ quiz-hub/src/routes/index.js
@@ -1,13 +1,6 @@
 import PrivateRoutes from "../components/PrivateRoutes";
 import LayoutDefault from "../layout/LayoutDefault";
-import Answers from "../pages/Answers";
 import Home from "../pages/Home";
-import Login from "../pages/Login";
-import Logout from "../pages/Logout";
-import Quiz from "../pages/Quiz";
-import Register from "../pages/Register";
-import Result from "../pages/Result";
-import Topic from "../pages/Topic";
 
 export const routes=[
     {
@@ -18,38 +11,15 @@ export const routes=[
             path:"/",
             element:<Home/>,
         },
-        {
-            path:"login",
-            element:<Login/>,
-        },
-        {
-            path:"register",
-            element:<Register/>
-        },
-        {
-            path:"logout",
-            element:<Logout/>
-        },
+        
         {
             
             element:<PrivateRoutes/>,
             children:[
                 {
-                    path:"answers",
-                    element:<Answers/>
+                    
                 },
-                {
-                    path:"topic",
-                    element:<Topic/>
-                },
-                {
-                    path:"quiz/:id",
-                    element:<Quiz/>
-                },
-                {
-                    path:"result/:id",
-                    element:<Result/>
-                },
+               
             ]
         },
        
