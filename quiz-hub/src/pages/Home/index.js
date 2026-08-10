eb33c500432c4649843d1de59b069f421fb7b095
pkhang35 <pkhang353@gmail.com>
2026-08-10 13:50:38 +0700

    add-contribute

--- quiz-hub/src/pages/Home/index.js
+++ quiz-hub/src/pages/Home/index.js
@@ -1,4 +1,4 @@
-import { Button, Tag, Statistic } from 'antd';
+import { Button, Tag, Statistic, Card } from 'antd';
 import {
     BookOutlined,
     CodeOutlined,
@@ -7,6 +7,8 @@ import {
     StarOutlined
 } from "@ant-design/icons";
 import heroImage from "../../assets/images/heroImage.png";
+import createQuiz from "../../assets/images/create-question.png";
+import createTopic from "../../assets/images/create-topics.png";
 import "./Home.scss"
 function Home(){
     return(
@@ -105,6 +107,64 @@ function Home(){
                    
                 </div>
             </section>
+
+            <section className="contribute">
+                <div className="container">
+                    <div className="contribute__list">
+                        <div className="contribute__card">
+                            <div className="contribute__images">
+                                <img src={createQuiz} alt="create-Quiz" />
+                            </div>
+
+                            <div className="contribute__content">
+                                <h3>
+                                    Tạo câu hỏi
+                                </h3>
+
+                                <p>
+                                    Thêm những câu hỏi lập trình thú vị
+                                    và giúp cộng đồng cùng luyện tập.
+                                </p>
+
+                                <Button
+                                    className="contribute__btn contribute__btn--quiz"
+                                >
+                                    Tạo câu hỏi
+                                    <ArrowRightOutlined />
+                                </Button>
+                            </div>
+                        </div>
+
+                        <div className="contribute__card">
+                            <div className="contribute__images">
+                                    <img src={createTopic} alt="create-Quiz" />
+                            </div>
+
+                            <div className="contribute__content ">
+
+                                <h3>
+                                    Tạo chủ đề
+                                </h3>
+
+                                <p>
+                                    Tạo một chủ đề mới và mở rộng
+                                    kho kiến thức của DevQuiz.
+                                </p>
+
+                                <Button
+                                    className="contribute__btn contribute__btn--topic"
+                                >
+                                    Tạo chủ đề
+                                    <ArrowRightOutlined />
+                                </Button>
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+            
         </>
     )
 }
